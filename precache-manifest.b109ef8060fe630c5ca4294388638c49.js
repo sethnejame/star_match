4040e6f7388d979324f3e888cb8657addea3880d
Seth NeJame <sethnejame@gmail.com>
2019-07-30 16:05:38 +0200

    Updates

---
+++ precache-manifest.b109ef8060fe630c5ca4294388638c49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1f6217a96e1cca565ecd4f6b733ee5",
+    "url": "/star_match/index.html"
+  },
+  {
+    "revision": "92b06956e53739dfa882",
+    "url": "/star_match/static/css/main.86ead799.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/star_match/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "92b06956e53739dfa882",
+    "url": "/star_match/static/js/main.eb6da86a.chunk.js"
+  },
+  {
+    "revision": "3b668875bde7617b501c",
+    "url": "/star_match/static/js/runtime~main.aa5c07b6.js"
+  }
+]);
